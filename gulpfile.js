9bc9328efbe9883f460fbf665ef1fdcc9d2dc634
Volodymyr Danyliv <volodymyr@1worldonline.com>
2015-12-22 13:51:52 +0200

    minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 var gulp = require('gulp'),
-    inlineCss = require('gulp-inline-css');
+    minifyCss = require('gulp-minify-css'),
+    inject = require('gulp-inject');
 
-gulp.task('default', function() {
-    return gulp.src('./index.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest('build/'));
+var filesToMove = [
+        'widget-factory.js',
+    ];
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(minifyCss({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('build/css'));
 });
+
+gulp.task('injectCss', ['minify-css'], function () {
+	gulp.src('templates/widget-content.tpl')
+		.pipe(inject(gulp.src(['build/css/style.css']), {
+			starttag: '<!-- inject:cssFile -->',
+			removeTags: true,
+			transform: function (filePath, file) {
+				return file.contents.toString('utf8')
+    	}
+		})
+	).pipe(gulp.dest('./build/templates'));
+});
+
+gulp.task('move', function(){
+  gulp.src(filesToMove, { base: './' })
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('prod', [ 'minify-css', 'injectCss', 'move']);
