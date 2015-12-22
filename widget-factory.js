199d448efd6cfd484aca8d26748bdc43478a034d
Volodymyr Danyliv <volodymyr@1worldonline.com>
2015-12-22 10:09:13 +0200

    frame builder | CORS wrapper | tpl actions | init listeners | method for tpl collections

--- widget-factory.js
+++ widget-factory.js
@@ -1,51 +1,183 @@
-;(function(root, factory){
-    if (!root.oneWorldWidget) root.oneWorldWidget = factory();
-
-    document.addEventListener('DOMContentLoaded', oneWorldWidget.initialize.bind(oneWorldWidget));
-
+;(function(root, factory) {
+    if (!root.oneWorldWidget) {
+        root.oneWorldWidget = factory();
+        document.addEventListener('DOMContentLoaded', oneWorldWidget.initialize.bind(oneWorldWidget));
+    }
 }(this, function() {
-    var widgetContainer = 'widget';
-
-    function getElement(id) {
-        return document.getElementById(id);
-    }
-
-    function getElements(className) {
-        return document.getElementsByClassName(className);
-    }
-
-    function ajax(dataObj) {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open('GET', dataObj.url, true);
-        xhr.send();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState != 4) return;
-
-            if (xhr.status != 200) {
-                console.log(xhr.status + ': ' + xhr.statusText);
-            }
-            else {
-                if (typeof dataObj.callbackSuccess === 'function') {
-                    dataObj.callbackSuccess(xhr.responseText);
-                }
-            }
+    var widgetListCode = [],
+        tplListener = {},
+        templates = {},
+        tplUrl = './templates/widget-content.tpl',
+        widgetParams = {
+            width: 200, //can be use with 'flexible' param
+            height: 300,
         };
-    }
 
-    function initListeners() {
-        var elements = getElements('vote'),
-            index = elements.length;
 
-        while (index--) {
-            elements[index].addEventListener('change', function() {
-                var sideId = this.getAttribute('data-side-id');
+    /*----------  init div/iframe  ----------*/
+    function findAllWidgetContainers() {
+        var selectOwoWidgetContainers = document.querySelectorAll('[data-owo-widget-type^=poll]');
 
-                sdk.pollVote(sideId);
-            });
+        for (var i = 0; i < selectOwoWidgetContainers.length; i++) {
+            selectOwoWidgetContainers[i].appendChild(addIframe());
         }
     }
 
+    function addIframe() {
+        var frame = document.createElement('iframe');
+
+        frame.setAttribute('id', addIdToken());
+        frame.setAttribute('scrolling', 'no');
+        frame.setAttribute('frameborder', '0');
+        frame.setAttribute('border', '0');
+        frame.frameBorder = '0';
+        frame.allowTransparency = 'true';
+        frame.setAttribute('style',
+                        'visibility: hidden; '+
+                        'height: 0px!important; '
+        );
+        frame.setAttribute('src', 'javascript:0;');
+        tplListener[frame.attributes.id.value] = 0;
+
+        return frame;
+    }
+
+    function addIdToken(div) {
+        var widgetContainer = div,
+            token = Math.floor(Math.random() * 50000) + 10000;
+        
+        widgetListCode.push('owo-' + token);
+
+        return 'owo-' + token;
+    }
+
+    /*----------  Ajax CORS wrapper  ----------*/
+    function createCORSrequest(method, url) {
+        var xhr = new XMLHttpRequest(),
+            xdr;
+
+        if ('withCredentials' in xhr) {
+            //support for modern browser
+            xhr.open(method, url, true);
+        }
+        else if (typeof XDomainRequest != undefined) {
+            //IE support
+            xdr = new XDomainRequest();
+            xdr.open(method, url);
+        }
+        else {responseText
+            //xhr not supported
+            xhr = null;
+        }
+
+        return xhr != undefined ? xhr : xdr;
+    }
+
+    function makeCorsRequest(objParams) {
+        var xhr = createCORSrequest(objParams.method, objParams.url);
+
+        if (!xhr) {
+            console.warn('CORS not supported!');
+            return;
+        }
+
+        xhr.onload = function() {
+            if (typeof objParams.callbackSuccess === 'function') {
+                objParams.callbackSuccess(xhr.responseText);
+            }
+            else {
+                console.log('Request without callback');
+            }
+        }
+
+        xhr.onerror = function() {
+            console.error('Request error');
+        }
+
+        xhr.send();
+    }
+
+    /*----------  prepare tpl for action  ----------*/
+    function prepareTpl(tpl) {
+        var re = /<tpl[\s\t]+id=\"((?!\")\w+)\"[\s\t]*>(((?!<\/tpl).)*)<\/tpl>/g;
+
+        tpl.replace(/(\r\n|\n|\r)/gm, "").replace(re, function(matchStr, id, template) {
+            templates[id] = template;
+        });
+
+        return templates;
+    }
+
+    function renderTpl() {
+        var frame;
+
+        for (var i = 0; i < widgetListCode.length; i++) {
+            frame = document.getElementById(widgetListCode[i]);
+            injectContent(frame);
+        }
+    }
+
+    function injectContent(frame, tplIndex) {
+        var iframeDocument = frame.contentDocument,
+            templateSize = Object.keys(templates),
+            tplCurrentIndex = tplIndex !== undefined ? tplIndex : 0;
+
+        iframeDocument.open();
+        iframeDocument.write(templates[templateSize[tplCurrentIndex]]);
+        iframeDocument.close();
+
+        if (frame.style.visibility !== '' || frame.style.height === '0px') {
+            setIframeStyle(frame);
+        }
+    }
+
+    function setIframeStyle(frame) {
+        var width,
+            height = widgetParams.height;
+
+        console.error(frame);
+        widgetParams.width === 'flexible' ? width = '100%' : width = widgetParams.width;
+        frame.setAttribute('style', 'width:'+ width + 'px; height: ' + height + 'px');
+    }
+
+    /*----------  init listeners  ----------*/
+
+    function initListeners() {
+        var iframe;
+
+        for (var i = 0; i < widgetListCode.length; i++) {
+            iframe = document.getElementById(widgetListCode[i]);
+        
+            if (iframe.contentDocument.getElementById('submit-poll') !== null) {
+                iframe.contentDocument.getElementById('submit-poll').addEventListener('click', clickListener, false);
+            }
+        }
+
+        function clickListener() {
+            if (validatePollForm()) {
+                showNextView();
+            }
+        }
+    }
+
+    /*----------  validate section  ----------*/
+    function validatePollForm() {
+        return true;
+    }
+
+    /*----------  tplCollection action  ----------*/
+    function showNextView() {
+        var frame = document.activeElement,
+            frametId = frame.attributes.id.value,
+            templateSize = Object.keys(templates);
+
+        if (tplListener[frametId] + 1 < templateSize.length) {
+            injectContent(frame, tplListener[frametId] + 1);
+            initListeners();
+            tplListener[frametId] += 1;
+        }
+    }
+    
     var sdk = {
         pollVote: function(sideId) {
             console.log('Voted: ', sideId);
@@ -56,34 +188,28 @@
         version: '0.0.1',
         widgetElement: null,
         initialize: function() {
-            console.error('widget was initialized');
-
-            this.render();
+            var self = this;
+            
+            findAllWidgetContainers();
+            self.render();
         },
         render: function() {
             var self = this;
 
-            self.widgetElement = getElement(widgetContainer);
-
-            ajax({
-                url: './templates/widget-content.tpl',
+            makeCorsRequest({
+                url: tplUrl,
+                method: 'GET',
                 callbackSuccess: function(response) {
                     try {
-                        self.widgetElement.innerHTML = response;
-
-                        initListeners.call(self);
+                        prepareTpl(response);
+                        renderTpl();
+                        initListeners();
                     }
                     catch (e) {
                         console.log('1W Error ' + e.name + ":" + e.message + "\n" + e.stack);
                     }
                 }
             });
-        },
-        hide: function () {
-            if (this.widgetElement) this.widgetElement.style.display = 'none';
-        },
-        show: function() {
-            if (this.widgetElement.style.display === 'none') this.widgetElement.style.display = 'block';
         }
     }
 }));
